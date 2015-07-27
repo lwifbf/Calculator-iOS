114cd50ce95e5c66beaeefd24c76bf43739850a0
lwifbf <lwifbf@gmail.com>
2015-07-27 16:32:25 +0800

    intitial commit

---
+++ Calculator/ViewController.swift
@@ -0,0 +1,128 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by apple on 2015/5/31.
+//  Copyright (c) 2015年 National Taiwan University. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var display: UILabel!
+    @IBOutlet weak var inputHistory: UILabel!
+    
+    var userHaveInsertedNumber: Bool = false
+    var haveInsertPoint: Bool = false
+    
+    var brain = CalculatorBrain()
+    
+    @IBAction func appendDigit(sender: UIButton) {
+        let digit = sender.currentTitle!
+        
+        inputHistory.text = inputHistory.text! + digit
+        
+        if digit == "." {
+            if !haveInsertPoint{
+                if userHaveInsertedNumber{
+                    display.text = display.text! + digit
+                }
+                else{
+                    display.text = "0."
+                    userHaveInsertedNumber = true
+                }
+                
+                haveInsertPoint = true
+            }
+        }
+            
+        else{
+            if userHaveInsertedNumber{
+                display.text = display.text! + digit
+            }
+            else{
+                display.text = digit
+                userHaveInsertedNumber = true
+            }
+        }
+
+//        println("digit = \(digit)")
+    }
+    
+    @IBAction func operate(sender: UIButton) {
+        let operation = sender.currentTitle!
+        
+        if userHaveInsertedNumber{
+            enter()
+        }
+        inputHistory.text = inputHistory.text! + operation
+        
+//        switch operation{
+//            
+//            //--Closure advanced use
+//            //func multiply(op1:D, op2:D) -> D {return op1*op2}
+//            //  performOp(multiply)
+//            //performOp( { (op1:D, op2:D) -> D in return op1*op2} )
+//            //performOp( { (op1, op2)  in return op1*op2} )
+//            //performOp( { (op1, op2)  in op1*op2} )
+//            //performOp( { $0*$1 } )
+//            
+//            case "✕":   performOperationBinary{ $1 * $0 }
+//            case "÷":   performOperationBinary{ $1 / $0 }
+//            case "+":   performOperationBinary{ $1 + $0 }
+//            case "−":   performOperationBinary{ $1 - $0 }
+//            case "√":   performOperationUnary{ sqrt($0) }
+//            case "sin":     performOperationUnary{ sin($0) }
+//            case "cos":     performOperationUnary{ cos($0) }
+//            case "π":   let pi = M_PI
+//                        displayValue = pi
+//                        enter()
+//            case "C":   inputHistory.text = ""
+//                        display.text = "0"
+//                        operandStack.removeAll()
+//            default :   break
+//        }
+    }
+    
+//    func performOperationBinary(operation: (Double,Double) -> Double){
+//        if operandStack.count>=2{
+//            displayValue = operation(operandStack.removeLast(),operandStack.removeLast())
+//            enter()
+//        }
+//    }
+//    
+//    func performOperationUnary(operation: Double -> Double){
+//        if operandStack.count>=1{
+//            displayValue = operation(operandStack.removeLast())
+//            enter()
+//        }
+//    }
+    
+    
+//    var operandStack = Array<Double>()
+    
+    
+    @IBAction func enter() {
+        userHaveInsertedNumber = false
+        haveInsertPoint = false
+        
+        brain.pushOperand(displayValue)
+//        operandStack.append(displayValue)
+//        println("operandStack = \(operandStack)")
+        
+        inputHistory.text = inputHistory.text! + " "
+    }
+    
+    var displayValue: Double {
+        get{
+            return NSNumberFormatter().numberFromString(display.text!)!.doubleValue
+        }
+        set{
+            display.text = "\(newValue)"
+//            userHaveInsertedNumber = false
+//            haveInsertPoint = false
+        }
+    }
+}
+
---
+++ CalculatorBrain.swift
@@ -0,0 +1,84 @@
+//
+//  CalculatorBrain.swift
+//  Calculator
+//
+//  Created by apple on 2015/7/23.
+//  Copyright (c) 2015年 National Taiwan University. All rights reserved.
+//
+
+import Foundation
+
+class CalculatorBrain{
+    
+    private enum Op{
+        case Operand(Double)
+        case UnaryOperation(String, Double -> Double)
+        case BinaryOperation(String, (Double,Double) -> Double)
+    }
+    
+    //Array<Op>() = [Op]()
+    private var opStack = [Op]()
+    
+    //Dictionary<String, Op>() = [Sreing:Op]()
+    private var knowOps = [String:Op]()
+    
+    init(){
+        
+        //Op.BinaryOperation("×", { $0*$1 })
+        //Op.BinaryOperation("×") { $0*$1 }
+        //Op.BinaryOperation("×", * )
+        
+        knowOps["×"] = Op.BinaryOperation("×", *)
+        knowOps["÷"] = Op.BinaryOperation("÷") { $1 / $0 }
+        knowOps["+"] = Op.BinaryOperation("+", +)
+        knowOps["−"] = Op.BinaryOperation("−") { $1 - $0 }
+        knowOps["√"] = Op.UnaryOperation("√", sqrt)
+    }
+    
+    private func evaluate(ops: [Op]) -> (result: Double?, remainingOps: [Op]){
+        
+        if !ops.isEmpty{
+            var remainingOps = ops
+            let op = remainingOps.removeLast()
+            
+            switch op{
+            case .Operand(let operand):
+                return (operand, remainingOps)
+                
+            case .UnaryOperation(_, let operation):
+                let operandEvaluation = evaluate(remainingOps)
+                if let operand = operandEvaluation.result{
+                    return (operation(operand), operandEvaluation.remainingOps)
+                }
+            case .BinaryOperation(_, let operation):
+                let op1Evaluation = evaluate(remainingOps)
+                if let op1 = op1Evaluation.result{
+                    
+                    let op2Evaluation = evaluate(op1Evaluation.remainingOps)
+                    if let op2 = op2Evaluation.result{
+                        return (operation(op1,op2), op2Evaluation.remainingOps)
+                    }
+                }
+            }
+        }
+        
+        return(nil, ops)
+    }
+    
+    func evaluate() -> Double?{
+        //remainder or _
+        let (result, remainingOps) = evaluate(opStack)
+        return result
+    }
+    
+    func pushOperand(operand: Double){
+        opStack.append(Op.Operand(operand))
+    }
+    
+    func performOperation(symbol: String){
+        if let operation = knowOps[symbol]{
+            opStack.append(operation)
+        }
+    }
+    
+}
